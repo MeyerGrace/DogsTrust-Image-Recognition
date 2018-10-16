b0fa3f2d376f23f93177a50655a21436ec3fe4cb
Grace Meyer <Grace.Meyer@oxera.com>
2018-10-16 17:33:18 +0100

    try to upload one image

--- customvision_files/dogvision-files.R
+++ customvision_files/dogvision-files.R
@@ -14,13 +14,14 @@
 
 library(httr)
 library(jsonlite)
+library(dplyr)
 setwd("~/GitHub/image-recognition-MS-CVai")
 
-## Read in a file of URLs of images of pugs, and also a file of grey hounds
-## The URLs were sourced on 6 October 2018
-## if in the future urls to fail or return thumbnail "errors", so a manual review will be necessary (see below).
-pugTrain <- scan("pugTrain.txt",what = character())
-greyhoundTrain <- scan("greyhoundTrain.txt", what = character())
+#breeds and location of files
+breedClasses <- c("greyhound", "pug")
+pugTrain <- "customvision_files/pug"
+greyhoundTrain <- "customvision_files/greyhound"
+
 
 ## Retrieve API keys from keys.txt file, set API endpoint
 keys <- read.table("keys.txt", header = TRUE, stringsAsFactors = FALSE)
@@ -28,7 +29,7 @@ keys <- read.table("keys.txt", header = TRUE, stringsAsFactors = FALSE)
 ## Check to see if the default keys.txt file is still there
 region <- keys["region", 1]
 if (region == "ERROR-EDIT-KEYS.txt-FILE") {
- stop("Edit the file keys.txt to provide valid keys. See README.md for details.")
+  stop("Edit the file keys.txt to provide valid keys. See David's README.md for details.")
 }
 
 ## retrieve custom vision key
@@ -39,18 +40,18 @@ cvision_api_endpoint <- "https://southcentralus.api.cognitive.microsoft.com/cust
 domainsURL <- paste0(cvision_api_endpoint, "/domains")
 
 APIresponse = GET(url = domainsURL,
-                   content_type_json(),
-                   add_headers(.headers = c('Training-key' = cvision_api_key)),
-                   body = "",
-                   encode = "json")
+                  content_type_json(),
+                  add_headers(.headers = c('Training-key' = cvision_api_key)),
+                  body = "",
+                  encode = "json")
 
 domains <- content(APIresponse)
 domains.Generic <- domains[[1]]$Id #this is where I select the generic domain
 
 ## Create a project
 createURL <- paste0(cvision_api_endpoint, "/projects?",
-                    "name=dogstrust2app&",
-                    'description=dogstrust2&',
+                    "name=DogFilesImages&",
+                    'description=DogFilesImages&',
                     'domainId=',
                     domains.Generic)
 
@@ -68,53 +69,74 @@ cvision_id <- content(APIresponse)$Id
 
 ## function to create one tag, and return its id
 createTag <- function(id, tagname) {
- eURL <- paste0(cvision_api_endpoint, "/projects/", id, "/tags?",
-                "name=",tagname)
-
- APIresponse = POST(url = eURL,
-                    content_type_json(),
-                    add_headers(.headers = c('Training-key' = cvision_api_key)),
-                    body = "",
-                    encode = "json")
-
- content(APIresponse)$Id
-}
-
-pug_tag <- createTag(cvision_id, "pug")
-greyhound_tag <- createTag(cvision_id, "greyhound")
-tags <- c(pug = pug_tag, greyhound = greyhound_tag)
-
-## Upload images to Custom Vision. We will cycle through lists of URLs
-## provided in the txt files
-
-uploadURLs <- function(id, tagname, urls) {
- ## id: Project ID
- ## tagname: one tag (applued to all URLs), as a tag ID
- ## urls: vector of image URLs
-
- eURL <- paste0(cvision_api_endpoint, "/projects/", id, "/images/url")
- success <- logical(0)
-
- ## The API accepts 64 URLs at a time, max, so:
- while (length(urls) > 0) {
-
-  N <- min(length(urls), 64)
-  urls.body <- toJSON(list(TagIds = tagname, Urls = urls[1:N]))
+  eURL <- paste0(cvision_api_endpoint, "/projects/", id, "/tags?",
+                 "name=",tagname)
 
   APIresponse = POST(url = eURL,
-                    content_type_json(),
-                    add_headers(.headers = c('Training-key' = cvision_api_key)),
-                    body = urls.body,
-                    encode = "json")
+                     content_type_json(),
+                     add_headers(.headers = c('Training-key' = cvision_api_key)),
+                     body = "",
+                     encode = "json")
 
-  success <- c(success,content(APIresponse)$IsBatchSuccessful)
-  urls <- urls[-(1:N)]
- }
- all(success)
+  content(APIresponse)$Id
 }
 
-uploadURLs(cvision_id, tags["pug"], pugTrain)
-uploadURLs(cvision_id, tags["greyhound"], greyhoundTrain)
+
+tags <- as.vector(0)
+for (i in 1:length(breedClasses)) {
+  #assign(paste0(i, "_tag"), createTag(projectid,i))
+  tag_i <- breedClasses[i]
+  tag_i
+  tags[i] <- createTag(cvision_id, tag_i)
+  tags[i]
+
+}
+names(tags) <- breedClasses
+
+# this is how you do it separately
+# pug_tag <- createTag(cvision_id, "pug")
+# greyhound_tag <- createTag(cvision_id, "greyhound")
+# tags <- c(pug = pug_tag, greyhound = greyhound_tag)
+
+## Upload images to Custom Vision. We will cycle through a folder of image files
+
+uploadFiles <- function(id, tagname, files) {
+  ## id: Project ID
+  ## tagname: one tag (applued to all URLs), as a tag ID
+  ## files: vector of image URLs
+
+  eFiles <- paste0(cvision_api_endpoint, "/projects/", id, "/images/files")
+  success <- logical(0)
+
+  ## The API accepts 64 URLs at a time, max, so:
+  while (length(files) > 0) {
+
+    N <- min(length(files), 64)
+    files.body <- toJSON(list(TagIds = tagname, images = files[1:N]))
+
+    APIresponse = POST(url = eFiles,
+                       content_type_json(),
+                       add_headers(.headers = c('Training-key' = cvision_api_key)),
+                       body = files.body,
+                       encode = "json")
+
+    success <- c(success,content(APIresponse)$IsBatchSuccessful)
+    files <- files[-(1:N)]
+  }
+  all(success)
+}
+
+#try for one image
+uploadFiles(cvision_id, tags["greyhound"], "customvision_files/greyhound/Greyhound (42).png")
+
+
+# for (i in breedClasses) {
+#   uploadURLs(cvision_id, tags[i], eval(parse(text = paste0(i,"Train"))))
+# }
+
+#how to do it one by one
+# uploadURLs(cvision_id, tags["pug"], pugTrain)
+# uploadURLs(cvision_id, tags["greyhound"], greyhoundTrain)
 
 ## If either of the calls above returned FALSE, that means at least one image
 ## couldn't be uploaded. This is most likely due to a bad URL, and the
@@ -131,10 +153,10 @@ uploadURLs(cvision_id, tags["greyhound"], greyhoundTrain)
 projURL <- paste0(cvision_api_endpoint, "/projects/")
 
 APIresponse = GET(url = projURL,
-                   content_type_json(),
-                   add_headers(.headers = c('Training-key' = cvision_api_key)),
-                   body = "",
-                   encode = "json")
+                  content_type_json(),
+                  add_headers(.headers = c('Training-key' = cvision_api_key)),
+                  body = "",
+                  encode = "json")
 
 projStatus <- content(APIresponse)
 
@@ -157,18 +179,18 @@ train.id <- content(APIresponse)$Id
 ## Function to check status of a trained model (iteration)
 
 iterStatus <- function(id) {
- iterURL <- paste0(cvision_api_endpoint, "/projects/",
+  iterURL <- paste0(cvision_api_endpoint, "/projects/",
                     cvision_id,
                     "/iterations/",
                     id)
 
- APIresponse = GET(url = iterURL,
+  APIresponse = GET(url = iterURL,
                     content_type_json(),
                     add_headers(.headers = c('Training-key' = cvision_api_key)),
                     body = "",
                     encode = "json")
 
- content(APIresponse)$Status
+  content(APIresponse)$Status
 }
 
 ## Training is asynchronous. Check until the status is "Completed"
@@ -195,45 +217,47 @@ cvision_pred_key <- keys["cvpred", 1]
 ## Function to generate predictions
 
 breed_predict <- function(imageURL, threshold = 0.5) {
- predURL <- paste0(cvision_api_endpoint_pred, "/", cvision_id,"/url?",
-                   "iterationId=",train.id,
-                   "&application=R"
-                   )
+  predURL <- paste0(cvision_api_endpoint_pred, "/", cvision_id, "/url?",
+                    "iterationId=", train.id,
+                    "&application=R"
+  )
 
- body.pred <- toJSON(list(Url = imageURL[1]), auto_unbox = TRUE)
+  body.pred <- toJSON(list(Url = imageURL[1]), auto_unbox = TRUE)
 
- APIresponse = POST(url = predURL,
-                    content_type_json(),
-                    add_headers(.headers= c('Prediction-key' = cvision_pred_key)),
-                    body = body.pred,
-                    encode = "json")
+  APIresponse = POST(url = predURL,
+                     content_type_json(),
+                     add_headers(.headers = c('Prediction-key' = cvision_pred_key)),
+                     body = body.pred,
+                     encode = "json")
 
- out <- content(APIresponse)
+  out <- content(APIresponse)
 
- if (!is.null(out$Code)) msg <- paste0("Can't analyze: ", out$Message) else
- {
-  predmat <- matrix(unlist(out$Predictions), nrow = 3)
-  preds <- as.numeric(predmat[3,])
-  names(preds) <- predmat[2,]
+  if (!is.null(out$Code)) msg <- paste0("Can't analyze: ", out$Message) else
+  {
+    predmat <- matrix(unlist(out$Predictions), nrow = 3)
+    preds <- as.numeric(predmat[3,])
+    names(preds) <- predmat[2,]
 
-  ## uncomment this to see the class predictions
-  ## print(preds)
+    ## uncomment this to see the class predictions
+    ## print(preds)
 
-  if (preds["pug"] > threshold) msg <- "Pug" else
-   if (preds["greyhound"] > threshold) msg <- "Greyhound" else
-    msg <- "Don't know"
+    if (max(preds) > threshold) {
+      msg <- names(preds)[which(preds == max(preds))]
+    } else {
+      msg <- "Unsure"
+    }
+
+    # if (preds["pug"] > threshold) msg <- "Pug" else
+    #  if (preds["greyhound"] > threshold) msg <- "Greyhound" else
+    #   msg <- "Don't know"
   }
 
-  names(msg) <- imageURL[1]
-  msg
+  msg <- c(imageURL[1], msg, max(preds))
+  return(msg)
 }
 
 breed_predict(pugTrain[1])
 breed_predict(greyhoundTrain[1])
-
-## here are some images to try, from a Google Image Search for "hotdog
-pugTest <- scan("pugTest.txt",what = character())
-greyhoundTest <- scan("greyhoundTest.txt",what = character())
 
 breed_predict(pugTest[1])
 breed_predict(greyhoundTest[1])
@@ -245,6 +269,31 @@ for (i in 1:length(pugTest)) {
 for (i in 1:length(greyhoundTest)) {
   print(breed_predict(greyhoundTest[i]))
 }
+
+testResults <- data.frame(label = "A",
+                          url = "A",
+                          prediction = "A",
+                          probability = "A",
+                          stringsAsFactors = FALSE)
+for (i in breedClasses) {
+  #print(i)
+  testUrls <- eval(parse(text = paste0(i,"Test")))
+  #print(testUrls)
+  for (j in testUrls) {
+    #print(j)
+    testPred <- breed_predict(j)
+    Sys.sleep(5)
+    #print(testPred)
+    testPred <- c(i, testPred)
+    #print(testPred)
+    testResults <- rbind(testResults, testPred)
+    #print(testResults)
+  }
+}
+
+testResults <- testResults[-1, ]
+
+accuracy <- mean(testResults$label == testResults$prediction)
 
 ## David has examples where the  classification is wrong, at the 50% threshold
 #so you can bump up the threshold to be higher
